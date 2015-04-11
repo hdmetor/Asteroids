956cb3abcb4ea1188113399efa6dbba0403770fe
hdmetor <hdmetor@gmail.com>
2015-04-11 17:08:37 -0400

    refactoring tutorial in more files

---
+++ init.cpp
@@ -0,0 +1,14 @@
+#include <allegro5/allegro.h>
+ 
+int init( float FPS = 60,  int SCREEN_W = 800,  int SCREEN_H = 600) {
+
+	if(!al_init()) {
+      fprintf(stderr, "failed to initialize allegro!\n");
+      return -1;
+   }
+
+    if(!al_install_keyboard()) {
+      fprintf(stderr, "failed to initialize the keyboard!\n");
+      return -1;
+   }
+}
---
+++ init.h
@@ -0,0 +1,8 @@
+#ifndef _INIT_FILE_
+#define _INIT_FILE_
+#include <allegro5/allegro.h>
+ 
+
+int init( float fps,  int width,  int height);
+
+#endif
